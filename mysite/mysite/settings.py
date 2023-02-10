b045e3b6921ecac72b2761f5eee52b4cd2301471
hang tran <hangmtran101@gmail.com>
2023-02-09 16:43:52 -0800

    changes settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -143,5 +143,5 @@ django_heroku.settings(locals())
 # }
 
 DATABASES = {
-  'default': dj_database_url.config(default=os.environ['DATABASE_URL'], conn_max_age=600)
+  'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'), conn_max_age=600)
 }
